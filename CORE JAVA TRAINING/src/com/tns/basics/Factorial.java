14ec7fff7412564896410790256ba4325a8cba17
Shweta <Shweta@SHWETA>
2022-07-07 20:51:58 +0530

    Java

---
+++ CORE JAVA TRAINING/src/com/tns/basics/Factorial.java
@@ -0,0 +1,19 @@
+package com.tns.basics;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		
+		int i, fact=1;
+		int number=5; //no. to calculate factorial
+		
+		for(i=1;i<=number;i++)
+		{
+			fact=fact*i;
+		}
+		
+		System.out.println("Factorial of "+number+" is : "+fact);
+
+	}
+
+}
